edb03f15f0cec728eee1f14100a3060c6ac6b861
rashedhasan7299 <rashedhasan7299@gmail.com>
2020-05-09 16:19:01 +0600

    done creating components

--- src/App.js
+++ src/App.js
@@ -3,24 +3,82 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person = {
+    name: "Steve Smith",
+    age: 35,
+    email: "steveSmith@gmail.com"
+  }
+
+  var person2 = {
+    name: "Will Smith",
+    age: 35,
+    email: "steveSmith@gmail.com"
+  }
+
+  var whiteBg = {
+    backgroundColor : "white",
+    color : "grey",
+    padding : "30px",
+    marginBottom : "20px"
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        
+        {/* <h4 style={whiteBg}>{ `Name: ${person.name}
+        Email: ${person.email}
+        Age: ${person.age}` }</h4> */}
+
+        {/* <h4 style = {{backgroundColor : "black"}}>{ `Name: ${person2.name}
+        Email: ${person2.email}
+        Age: ${person2.age}` }</h4> */}
+        
+        {/* <p>Paragraph done in React</p> */}
       </header>
     </div>
   );
+}
+
+function Person() {
+
+  const personStyle = {
+    backgroundColor : "white",
+    color : "#333",
+    padding : "20px",
+    marginBottom : "10px",
+    border: "1px solid darkGrey"
+  }
+
+  const buttonStyle = {
+    padding : "15px",
+    backgroundColor : "#333",
+    color : "#fff",
+    border : "none",
+    borderRadius : "5px",
+    fontWeight : "600",
+    fontSize : "1rem"
+  }
+
+  const buttonStyleHover = {
+    backgroundColor : "#111",
+    color : "#fff",
+    borderRadius : "50px",
+  }
+
+  function clickHandle (e) {
+    e.preventDefault();
+    alert("Yes, it is true!");
+  }
+  return(
+    <div style = {personStyle}>
+      <h1>Sakib Al Hasan</h1>
+      <button style = {buttonStyle} onMouseEnter = {buttonStyleHover} onClick = {clickHandle}>Best all rounder in the world</button>
+    </div>
+  )
 }
 
 export default App;
